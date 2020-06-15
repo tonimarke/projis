37e2d029adae580451a89d5108d9ff9682738ca8
Carlos Gomes <carlos.henrique@unesp.br>
2020-06-14 22:52:31 -0300

    inicio frontend e modulos angular

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -5,10 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
+import { MainToolbarComponent } from './ui/main-toolbar/main-toolbar.component';
+import { MainMenuComponent } from './ui/main-menu/main-menu.component';
+import { MainFooterComponent } from './ui/main-footer/main-footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainToolbarComponent,
+    MainMenuComponent,
+    MainFooterComponent
   ],
   imports: [
     BrowserModule,
